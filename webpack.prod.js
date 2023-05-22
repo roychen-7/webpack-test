ad17b30143c78203030e18cfda4744c277caf12c
chenyou.roy <chenyou.roy@bytedance.com>
2023-05-22 22:31:56 +0800

    /

--- webpack.prod.js
+++ webpack.prod.js
@@ -197,6 +197,29 @@ module.exports = {
               // limit: 100000,
             },
           },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
         ],
       },
     ],
